e8233709323d97748b5baa2b9954f110f8e0c175
Timothy R. Butler <tbutler@ofb.biz>
2024-07-17 22:59:31 -0500

    Polishing up.

--- syncNginxProxy.pl
+++ syncNginxProxy.pl
@@ -4,72 +4,97 @@ use strict;
 use warnings;
 use Apache::ConfigFile;
 use Template;
+use Getopt::Long;
 
-# Open the Apache configuration file for reading
-my $parameters = {};
-$parameters->{'VirtualminServer'} = shift @ARGV;
+my ($target, $create, $delete, $test, $modification);
 
-# Use the Apache::ConfigFile module to parse the Apache configuration file
-my $apacheConfig = Apache::ConfigFile->read('/etc/apache2/sites-available/' . $parameters->{'VirtualminServer'} . '.conf');
+GetOptions ("target|t=s" 			=> \$target,
+			"create|modify|c|m"     => \$create,
+            "delete|d"              => \$delete,
+            "test"                  => \$test );
 
-use Data::Dumper;
+# Do we have settings from Virtualmin?
+if ($ENV{'VIRTUALSERVER_ACTION'}) {
+    $delete //= (any { $_ eq $ENV{'VIRTUALSERVER_ACTION'} } [ 'DELETE_DOMAIN', 'DISABLE_DOMAIN' ]) ? 1 : 0;
+    $create //= (any { $_ eq $ENV{'VIRTUALSERVER_ACTION'} } [ 'CREATE_DOMAIN', 'MODIFY_DOMAIN', 'CLONE_DOMAIN', 'ENABLE_DOMAIN' ]) ? 1 : 0;
 
-for my $vh ($apacheConfig->cmd_context('VirtualHost')) {
-    my $vhost = $apacheConfig->cmd_context('VirtualHost' => $vh);
-    
-    # Collect virtual domains
-    my @serverNames = $vhost->cmd_config('ServerAlias');
-    push (@serverNames, $vhost->cmd_config('ServerName'));
-    $parameters->{'server_name'} = join(' ', @serverNames);
-    $parameters->{'root'} = $vhost->cmd_config('DocumentRoot');
+    $target //= $ENV{'VIRTUALSERVER_DOM'};
+}
 
-    # SSL Parameters
-    $parameters->{'ssl_certificate'} = $vhost->cmd_config('SSLCertificateFile');
-    $parameters->{'ssl_certificate_key'} = $vhost->cmd_config('SSLCertificateKeyFile');
+# Require a target.
+die "No target specified" unless $target;
 
-    # foreach (@serverNames) {
-    #     print $_ . "\n";
-    # }
+# See if file exists.
+unless (-e '/etc/apache2/sites-available/' . $target) {
+    die "Configuration file doesn't exist.";
+}
 
-   # my $vhost_server_name = $vh->cmd_config('ServerName');
-   # my $vhost_doc_root    = $vh->cmd_config('DocumentRoot');
-} 
+if ($create) {
+    # Open the Apache configuration file for reading
+    my $parameters = {};
+    $parameters->{'TargetConfig'} = $target;
+    $parameters->{'action'} = $action // 'create';
 
-# Produce template
-my $template = Template->new();
-my $output;
-$template->process('nginxProxyTemplate.tt', $parameters, '/etc/nginx/sites-available/' . $parameters->{'VirtualminServer'}) || die $template->error();
+    $VIRTUALSERVER_DOM;
 
-#print $output;
+    # Use the Apache::ConfigFile module to parse the Apache configuration file
+    my $apacheConfig = Apache::ConfigFile->read('/etc/apache2/sites-available/' . $parameters->{'TargetConfig'} . '.conf');
 
-# # Access the parameters in the Apache configuration file
-# my $hostname = $apacheConfig->cmd_config('ServerName');
-# my $doc_root = $apacheConfig->cmd_config('DocumentRoot');
+    for my $vh ($apacheConfig->cmd_context('VirtualHost')) {
+        my $vhost = $apacheConfig->cmd_context('VirtualHost' => $vh);
+        
+        # Collect virtual domains
+        my @serverNames = $vhost->cmd_config('ServerAlias');
+        push (@serverNames, $vhost->cmd_config('ServerName'));
+        $parameters->{'server_name'} = join(' ', @serverNames);
+        $parameters->{'root'} = $vhost->cmd_config('DocumentRoot');
 
-# my $directive = $config->directive;
-# my $value = $config->value;
+        # SSL Parameters
+        $parameters->{'ssl_certificate'} = $vhost->cmd_config('SSLCertificateFile');
+        $parameters->{'ssl_certificate_key'} = $vhost->cmd_config('SSLCertificateKeyFile');
 
-# # Perform your conversion logic using the directive and value variables
+        # foreach (@serverNames) {
+        #     print $_ . "\n";
+        # }
 
-# # Example conversion logic: Convert "Listen" directive to "listen" directive
-# if ($directive eq 'Listen') {
-#     $directive = 'listen';
-# }
+    # my $vhost_server_name = $vh->cmd_config('ServerName');
+    # my $vhost_doc_root    = $vh->cmd_config('DocumentRoot');
+    } 
 
+    # Produce template
+    my $template = Template->new();
+    my $output;
 
+    if ($test) {
+        $template->process('nginxProxyTemplate.tt', $parameters, \$output) || die $template->error();
+        say STDOUT $output;
+        exit;
+    } else {
+        $template->process('nginxProxyTemplate.tt', $parameters, '/etc/nginx/sites-available/' . $parameters->{'TargetConfig'}) || die $template->error();
+        say STDOUT "Nginx configuration file created or modified successfully.";
+        $modification = 1;
+    }
+}
 
+if ($delete) {
+    if ($test) {
+        say STDOUT "Nginx configuration file would be deleted successfully.";
+        exit;
+    }
+    else {
+        unlink '/etc/nginx/sites-available/' . $target;
+        say STDOUT "Nginx configuration file deleted successfully.";
+        $modification = 1;
+    }
+}
 
-# # Open the NGINX configuration file for writing
-# open(my $nginx_fh, '>', 'nginx.conf') or die "Failed to open NGINX configuration file: $!";
+# See if we need to restart NGINX
+if ($modification) {
+    my $result = system('service nginx restart');
 
-#     # Write the converted line to the NGINX configuration file
-#     print $nginx_fh "$directive $value;\n";
-
-#     # Write the converted line to the NGINX configuration file
-#     print $nginx_fh $line;
-# }
-
-# # Close the file handles
-# close($nginx_fh);
-
-# print "Conversion completed successfully!\n";
+    if ($result == 0) {
+        say STDOUT "NGINX restarted successfully.";
+    } else {
+        say STDOUT "Failed to restart NGINX.";
+    }
+}
