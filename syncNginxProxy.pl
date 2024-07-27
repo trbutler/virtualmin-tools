09ee403278a823752d08cc2b799699c5f31cc665
Timothy R. Butler <tbutler@ofb.biz>
2024-07-27 02:05:16 -0500

    Bug fix

--- syncNginxProxy.pl
+++ syncNginxProxy.pl
@@ -286,7 +286,7 @@ sub proxyControl {
     # Open the Apache port configuration for adjustments. 
     open (my $fh, '+<', $targetFile) or die "Could not open file '$targetFile' $!";
     my $fileContent = do { local $/; <$fh> };
-    $fileContent =~ s/(?<=\:| )($ports->{$presentState}|$SSLports->{$presentState})/($2 eq $ports->{$presentState}) ? $ports->{$targetState} : $SSLports->{$targetState}/ge;
+    $fileContent =~ s/(?<=\:| )($ports->{$presentState}|$SSLports->{$presentState})/($1 eq $ports->{$presentState}) ? $ports->{$targetState} : $SSLports->{$targetState}/ge;
 
     print $fileContent;
 
