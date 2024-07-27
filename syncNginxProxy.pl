e87032ebe58e9cf8c6e56067426a4e7fbdbbd068
Timothy R. Butler <tbutler@ofb.biz>
2024-07-27 01:07:28 -0500

    Working on proxy control.

--- syncNginxProxy.pl
+++ syncNginxProxy.pl
@@ -260,7 +260,7 @@ sub proxyControl {
     my $apacheConfig = Apache::ConfigFile->read('/etc/apache2/ports.conf');
 
     # Get all Listen directives
-    my @listen_directives = $apacheConfig->cmd("Listen");
+    my @listen_directives = $apacheConfig->cmd_config_array("Listen");
 
     # Print all Listen directives
     foreach my $directive (@listen_directives) {
